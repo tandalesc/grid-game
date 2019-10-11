177205306a96d75f0cda5466050794fdf6522185
Shishir Tandale <shishir@tandale.com>
2019-10-11 03:26:43 -0400

    added metroid like weapon and directional aiming

--- src/player.rs
+++ src/player.rs
@@ -10,7 +10,13 @@ pub struct Player {
     pub jump_counter: u32,
     pub jump_timer: u32,
     pub position: Vector2,
-    pub velocity: Vector2
+    pub velocity: Vector2,
+    pub shoot_timer: u32,
+    pub charging_time: u32,
+    pub arm_offset: f32,
+    pub arm_direction: Vector2,
+    pub facing_direction: Vector2,
+    pub is_aiming: bool
 }
 impl Player {
     pub fn new() -> Player {
@@ -18,11 +24,25 @@ impl Player {
             jump_counter: 100,
             jump_timer: 0,
             position: Vector2::new(20., 20.),
-            velocity: Vector2::new(0., 0.)
+            velocity: Vector2::new(0., 0.),
+            shoot_timer: 0,
+            charging_time: 0,
+            arm_offset: 8.,
+            arm_direction: Vector2::new(1., 0.),
+            facing_direction: Vector2::new(1., 0.),
+            is_aiming: false
         }
+    }
+    pub fn aim_in_direction(&mut self, direction: Vector2) {
+        self.facing_direction.x = direction.x.signum();
+        self.arm_direction = {
+            let new_angle = direction.y.atan2(direction.x);
+            Vector2::new(new_angle.cos(), new_angle.sin())
+        };
     }
     pub fn update(&mut self, ctx: &mut ggez::Context) -> ggez::GameResult {
         let dt = timer::duration_to_f64(timer::delta(ctx)) as f32;
+        let mut friction_multiplier = 1.;
         let mut new_pos = self.position + self.velocity*dt;
         let mut new_vel = self.velocity.clone();
         //constrain position on edge of world
@@ -34,15 +54,22 @@ impl Player {
             new_pos.y = new_pos.y.max(0.).min(WORLD_SIZE.1 - PLAYER_SIZE.1);
             new_vel.y = 0.;
             self.jump_counter = 0;
+            if self.is_aiming {
+                friction_multiplier = 10.;
+            }
         }
         if new_pos.y + PLAYER_SIZE.1 < WORLD_SIZE.1 {
             new_vel.y += 200.*dt;
         }
         new_vel += (new_pos - self.position)*dt;
-        new_vel -= FRICTION*self.velocity*dt;
+        new_vel -= FRICTION*self.velocity*dt*friction_multiplier;
         self.position = new_pos;
         self.velocity = new_vel;
 
+
+        if self.shoot_timer > 0 {
+            self.shoot_timer -= 1;
+        }
         if self.jump_timer > 0 {
             self.jump_timer -= 1;
         }
--- src/state.rs
+++ src/state.rs
@@ -1,4 +1,5 @@
 
+use ggez::timer;
 use ggez::event;
 use ggez::graphics;
 use ggez::nalgebra as na;
@@ -17,13 +18,25 @@ const CAMERA_FOLLOW_SPEED: f32 = 0.2;
 
 const BG_TILE_SIZE: (f32, f32) = (20., 20.);
 pub const WORLD_SIZE: (f32, f32) = (320., 120.);
-pub const PLAYER_SIZE: (f32, f32) = (10., 10.);
+pub const PLAYER_SIZE: (f32, f32) = (10., 18.);
 
 pub const FRICTION: f32 = 1.;
+
+struct Bullet {
+    center: Vector2,
+    half_extents: Vector2,
+    velocity: Vector2,
+    damage: f32,
+    bullet_type: BulletType
+}
+enum BulletType {
+    Player, Enemy
+}
 
 pub struct MainState {
     player: Player,
     camera_target: Vector2,
+    bullets: Vec<Bullet>,
     input_events: HashSet<event::KeyCode>
 }
 impl MainState {
@@ -31,30 +44,54 @@ impl MainState {
         let ms = MainState {
             player: Player::new(),
             camera_target: Vector2::new(0., 0.),
+            bullets: Vec::new(),
             input_events: HashSet::new()
         };
         Ok(ms)
     }
     pub fn process_inputs(&mut self) -> ggez::GameResult {
         //use a hashset for consistent input polling
+        let mut player_aim_dir = Vector2::new(0., 0.);
         for key in &self.input_events {
             match key {
-                event::KeyCode::Space => {
+                event::KeyCode::Z => {
                     //allow double jumps with some separation
                     if self.player.jump_counter < 2 && self.player.jump_timer == 0 {
-                        self.player.velocity.y = -100.0;
+                        self.player.velocity.y = -150.0;
                         self.player.jump_timer = 20;
                         self.player.jump_counter += 1;
                     }
                 },
+                event::KeyCode::X => {
+                    //charge weapon
+                    if self.player.charging_time < 100 {
+                        self.player.charging_time += 1;
+                    }
+                },
                 event::KeyCode::Left => {
-                    self.player.velocity.x -= 2.0;
+                    if !self.player.is_aiming {
+                        self.player.velocity.x -= 2.0;
+                    }
+                    player_aim_dir.x -= 1.;
                 },
                 event::KeyCode::Right => {
-                    self.player.velocity.x += 2.0;
+                    if !self.player.is_aiming {
+                        self.player.velocity.x += 2.0;
+                    }
+                    player_aim_dir.x += 1.;
                 },
+                event::KeyCode::Up => {
+                    player_aim_dir.y -= 1.;
+                }
+                event::KeyCode::Down => {
+                    player_aim_dir.y += 1.;
+                }
                 _ => { /* do nothing */ }
             }
+        }
+        //aim player's gun in the appropriate direction
+        if player_aim_dir.norm() > 0. {
+            self.player.aim_in_direction(player_aim_dir/player_aim_dir.norm());
         }
         Ok(())
     }
@@ -71,12 +108,28 @@ impl MainState {
             .min(WORLD_SIZE.1.max(CAMERA_RESOLUTION.1) - CAMERA_RESOLUTION.1/2.);
         Ok(())
     }
+    pub fn update_bullets(&mut self, ctx: &mut ggez::Context) -> ggez::GameResult {
+        let dt = timer::duration_to_f64(timer::delta(ctx)) as f32;
+        //move all bullets
+        for bullet in &mut self.bullets {
+            bullet.center += bullet.velocity*dt;
+        }
+        //remove those that are off-screen
+        self.bullets.retain(|bullet| {
+            bullet.center.x - bullet.half_extents.x > 0. &&
+            bullet.center.y - bullet.half_extents.y > 0. &&
+            bullet.center.x + bullet.half_extents.x < WORLD_SIZE.0 &&
+            bullet.center.y + bullet.half_extents.y < WORLD_SIZE.1
+        });
+        Ok(())
+    }
 }
 impl event::EventHandler for MainState {
     fn update(&mut self, ctx: &mut ggez::Context) -> ggez::GameResult {
         self.process_inputs()?;
-        self.update_camera()?;
         self.player.update(ctx)?;
+        self.update_bullets(ctx)?;
+        self.update_camera()?;
         Ok(())
     }
     fn draw(&mut self, ctx: &mut ggez::Context) -> ggez::GameResult {
@@ -111,11 +164,30 @@ impl event::EventHandler for MainState {
             }
         }
 
+        for bullet in &self.bullets {
+            mesh_builder.rectangle(
+                graphics::DrawMode::fill(),
+                graphics::Rect::new(
+                    bullet.center.x-bullet.half_extents.x, bullet.center.y-bullet.half_extents.y,
+                    bullet.half_extents.x*2., bullet.half_extents.y*2.
+                ),
+                graphics::Color::from_rgba(0, 0, 0, 255)
+            );
+        }
+
         //create player
         mesh_builder.rectangle(
             graphics::DrawMode::fill(),
             graphics::Rect::new(self.player.position.x, self.player.position.y, PLAYER_SIZE.0, PLAYER_SIZE.1),
             graphics::Color::from_rgba(0, 0, 0, 255)
+        );
+        //create player gun; change color depending on charge
+        let gun_pos = self.player.position + Vector2::new(PLAYER_SIZE.0-5.,PLAYER_SIZE.1-5.)/2. + self.player.arm_direction*self.player.arm_offset;
+        let gun_brightness_u8 = interpolate_u8(0, 128, ((self.player.charging_time as f32)/100. + 1.).ln());
+        mesh_builder.rectangle(
+            graphics::DrawMode::fill(),
+            graphics::Rect::new(gun_pos.x, gun_pos.y, 5., 5.),
+            graphics::Color::from_rgba(gun_brightness_u8, gun_brightness_u8, gun_brightness_u8, 255)
         );
 
         //build and draw all meshes
@@ -126,13 +198,43 @@ impl event::EventHandler for MainState {
         Ok(())
     }
     fn key_down_event(&mut self, _ctx: &mut ggez::Context, keycode: event::KeyCode, _keymods: event::KeyMods, _repeat: bool) {
+        match keycode {
+            event::KeyCode::LShift => {
+                self.player.is_aiming = true;
+            },
+            _ => { /* do nothing */ }
+        };
         self.input_events.insert(keycode);
     }
     fn key_up_event(&mut self, _ctx: &mut ggez::Context, keycode: event::KeyCode, _keymods: event::KeyMods) {
+        match keycode {
+            event::KeyCode::LShift => {
+                self.player.is_aiming = false;
+            },
+            event::KeyCode::X => {
+                //fire gun with appropriate strength depending on charge
+                if self.player.shoot_timer == 0 {
+                    self.player.shoot_timer = 10;
+                    let bullet_size = interpolate_f32(1., 4., (self.player.charging_time as f32)/100.);
+                    self.bullets.push(Bullet {
+                        center: self.player.position + Vector2::new(PLAYER_SIZE.0, PLAYER_SIZE.1)/2. + self.player.arm_direction*(self.player.arm_offset + bullet_size),
+                        half_extents: Vector2::new(bullet_size, bullet_size),
+                        velocity: self.player.arm_direction/self.player.arm_direction.norm()*(160. + self.player.velocity.norm()),
+                        damage: 10.*bullet_size,
+                        bullet_type: BulletType::Player
+                    });
+                    self.player.charging_time = 0;
+                }
+            },
+            _ => { /* do nothing */ }
+        };
         self.input_events.remove(&keycode);
     }
 }
 
 fn interpolate_u8(start: u8, end: u8, fade: f32) -> u8 {
     ((start as f32)*(1.-fade) + (end as f32)*fade) as u8
+}
+fn interpolate_f32(start: f32, end: f32, fade: f32) -> f32 {
+    start*(1.-fade) + end*fade
 }
